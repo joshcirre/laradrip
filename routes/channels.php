ee923e46f2d9347edb0da34cd0bc43f7879f3c34
Josh Cirre <joshcirre@gmail.com>
2025-08-29 17:30:02 -0700

    pusher

--- routes/channels.php
+++ routes/channels.php
@@ -2,11 +2,5 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
-
-// Public channel for images - everyone can see all images
-Broadcast::channel('images', function () {
-    return true;
-});
+// We're using a public channel for images since everyone can see all images
+// No authorization needed for public channels in this app
